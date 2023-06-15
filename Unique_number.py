15e9a17de92e9d77d2be846f6c2f08b0b9c330b8
Sindhu Thiriveedhi <120095796+22p31a0550@users.noreply.github.com>
2023-06-15 10:05:02 +0530

    n=int(input()) s=str(n) unique=[int(x) for x in set(str(n))] if len(unique)==len(s):     print("Unique Number") else:     print("Not Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,6 +1,7 @@
-n=input()
-l=set(n)
-if len(n)==len(l):
+n=int(input())
+s=str(n)
+unique=[int(x) for x in set(str(n))]
+if len(unique)==len(s):
     print("Unique Number")
 else:
     print("Not Unique Number")
